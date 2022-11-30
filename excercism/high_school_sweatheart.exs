1aa645ed656c6adabf14f0499126c1502efdd14d
AbdullahAtique <abdullah.atique@invozone.com>
2022-11-30 13:10:39 +0500

    recursion and function_caluse practice

---
+++ excercism/high_school_sweatheart.exs
@@ -0,0 +1,33 @@
+defmodule HighSchoolSweetheart do
+  def first_letter(name) do
+    String.trim(name) |> String.first
+  end
+
+  def initial(name) do
+    String.upcase(first_letter(name)) <> "."
+  end
+
+  def initials(full_name) do
+    [first, last] = String.split(full_name, " ")
+    "#{initial(first)} " <> "#{initial(last)}"
+  end
+
+  def pair(full_name1, full_name2) do
+    """
+         ******       ******
+       **      **   **      **
+     **         ** **         **
+    **            *            **
+    **                         **
+    **     #{initials(full_name1)}  +  #{initials(full_name2)}     **
+     **                       **
+       **                   **
+         **               **
+           **           **
+             **       **
+               **   **
+                 ***
+                  *
+    """
+  end
+end
